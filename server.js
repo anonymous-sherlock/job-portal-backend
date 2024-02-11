623cef73705107591d3539f21bda79b0907db5f6
piyush-kumar-agarwal <piyush.7434@gmail.com>
2024-02-11 22:32:46 +0530

    cors update

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const app = require('./app');
 const DbConnect = require('./Database/Database');
 const PORT= process.env.PORT || 8800 ;
-const cloudinary  = require('cloudinary')
 // database connection 
 DbConnect();
-cloudinary.v2.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_SECRET,
-    api_secret:process.env.CLOUDINARY_API_KEY
+app.use('/',(req,res)=>{
+res.send("Welcome")
 })
 app.listen(PORT,()=>{
     console.log(`server is running on http//localhost:${PORT}`);
